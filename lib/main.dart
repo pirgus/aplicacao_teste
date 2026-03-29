2af4c7ec0b27fef69b4c582d2a31365fa087c35c
pirgus <jaquecavaller@gmail.com>
2026-03-29 20:46:36 -0300

    criacao do stateful widget para home page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -13,19 +13,42 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.purpleAccent,
-          title: Center(
-            child: Text("nome do meu app")
-            ),
+          title: Center(child: Text("nome do meu app")),
         ),
         body: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("olá, mundo!"),
-              Icon(Icons.add_location_sharp)
-              ],
+            children: [Text("olá, mundo!"), Icon(Icons.add_location_sharp)],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<StatefulWidget> {
+  String texto = "olá, mundo!";
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(texto),
+          ElevatedButton(onPressed: () {
+            setState(() {
+              texto = "voce mudou o texto!";
+            });
+          }, child: Text("clique em mim!"))
+          ],
       ),
     );
   }
